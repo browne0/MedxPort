952b053f8b552e9d42252a3eaed369014357cf9a
Malik <browne0@purdue.edu>
2016-02-20 03:06:57 -0500

    idk whats going on

--- iosApp/MedxPort/Pods/Target Support Files/Pods-MedxPort/Pods-MedxPort-frameworks.sh
+++ iosApp/MedxPort/Pods/Target Support Files/Pods-MedxPort/Pods-MedxPort-frameworks.sh
@@ -85,9 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-MedxPort/AFNetworking.framework"
+  install_framework "Pods-MedxPort/Firebase.framework"
   install_framework "Pods-MedxPort/JVFloatLabeledTextField.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-MedxPort/AFNetworking.framework"
+  install_framework "Pods-MedxPort/Firebase.framework"
   install_framework "Pods-MedxPort/JVFloatLabeledTextField.framework"
 fi
