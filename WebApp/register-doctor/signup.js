647fa08ac2deda5df5679d15f106271d8e8ad895
Malik <browne0@purdue.edu>
2016-02-20 10:38:01 -0500

    dropdown for clinics

--- WebApp/register-doctor/signup.js
+++ WebApp/register-doctor/signup.js
@@ -41,6 +41,20 @@ $(function(){
 
 app.controller("RegisterCtrl", ["$scope", "Auth",
 	function($scope, Auth) {
+		var ref = new Firebase("https://medxport.firebaseio.com/Clinics/");
+		$scope.clinics = [];
+		ref.on("value", function(snapshot) {
+			snapshot.forEach(function(childSnapshot) {
+		    // key will be "fred" the first time and "barney" the second time
+		    var key = childSnapshot.key();
+		    // childData will be the actual contents of the child
+		    var childData = childSnapshot.val();
+		    $scope.clinics.push(childData.clinicName);
+		  });
+			console.log($scope.clinics);
+		}, function (errorObject) {
+			console.log("The read failed: " + errorObject.code);
+		});
 		$scope.createUser = function() {
 			$scope.message = null;
 			$scope.error = null;
