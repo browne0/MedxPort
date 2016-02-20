eae5818e1729f5f851d545b31cdce5252431654a
Shang Zhou Xia <shangxzx@gmail.com>
2016-02-20 14:52:12 -0500

    DoctorCal

--- WebApp/doctor/doctorCal.js
+++ WebApp/doctor/doctorCal.js
@@ -1,61 +1,19 @@
-var State = {
-    CHECKED: 'checked',
-    UNCHECKED: 'unchecked',
-    NONE: 'none'
-};
+$(function () {
+  var isMouseDown = false;
+  $("#our_table td")
+    .mousedown(function () {
+      isMouseDown = true;
+      $(this).toggleClass("highlighted");
+      return false; // prevent text selection
+    })
+    .mouseover(function () {
+      if (isMouseDown) {
+        $(this).toggleClass("highlighted");
+      }
+    });
 
-states = [];
-
-$.each(State, function(k, state) {
-    states.push(state);
-});
-
-function getState(el$) {
-    return el$.data('state') || State.UNCHECKED;
-}
-function setState(el$, state) {
-    var oldState = getState(el$);
-    el$.data('state', state)
-        .removeClass(oldState)
-        .addClass(state);
-}
-
-function onDrop(event, ui) {
-    var droppable$ = $(this),
-        newState = getState(ui.draggable);
-    setState(droppable$, newState);
-}
-
-function onClick() {
-    var checkbox$ = $(this),
-        state = getState(checkbox$);
-    stateIndex = states.indexOf(state) + 1;
-    if(stateIndex > states.length - 1) {
-        stateIndex = 0;
-    }
-    currentState = states[stateIndex];
-    setState(checkbox$, currentState);
-}
-
-function onDragStart() {
-    $(this).off('click', onClick);
-}
-
-function onDragStop() {
-    $(this).on('click', onClick);
-}
-
-$('.checkbox')
-.on('click', onClick)
-.draggable({
-    revert: true,
-    revertDuration: 200,
-    delay: 100,
-    start: onDragStart,
-    stop: onDragStop
-})
-.droppable({
-    accept: '.checkbox',
-    drop: onDrop,
-    over: onDrop
+  $(document)
+    .mouseup(function () {
+      isMouseDown = false;
+    });
 });
