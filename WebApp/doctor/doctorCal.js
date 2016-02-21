f7fd74c1e1bbbc4e7cf8abc78546af8a7218080d
Shang Zhou Xia <shangxzx@gmail.com>
2016-02-20 22:00:55 -0500

    Doctor

--- WebApp/doctor/doctorCal.js
+++ WebApp/doctor/doctorCal.js
@@ -1,6 +1,21 @@
 $(function () {
   var isMouseDown = false;
-  $("#our_table td")
+
+  var days = new Array(31);
+  for (i=0;i<days.length;i++)
+  {
+    days[i] = (i+1).toString();
+  }
+
+  var elements = days.map(function (day) {
+    var el = $('<td>');
+    el.text(day);
+    return el;
+  });
+
+  $('#row').append(elements);
+
+  $("#our_table tr td")
     .mousedown(function () {
       isMouseDown = true;
       $(this).toggleClass("highlighted");
@@ -16,4 +31,9 @@ $(function () {
     .mouseup(function () {
       isMouseDown = false;
     });
+    // func left7()
+    // {
+    //   for(int i=1;i<)
+    //   document.innerHTMLByID("a + i") =
+    // }
 });
