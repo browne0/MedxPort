38490d527a9b3dd4853b3c28df8b032dfce11890
Eugene Chu <echu31@gatech.edu>
2016-02-21 06:47:17 -0500

    doctor kindanot really

--- WebApp/doctor/doctorCal.js
+++ WebApp/doctor/doctorCal.js
@@ -14,6 +14,7 @@ jQuery(document).ready(function() {
   $('#row').append(elements);
 });
 
+
 $(function () {
   var isMouseDown = false;
   $("#doctable tr td")
@@ -25,7 +26,9 @@ $(function () {
          var hl = $("highlighted");
          // hl.id = "ghl";
 
-
+      //  $("Available").click(function(){
+      //   $("highlighted").toggleClass("green");
+      // }
 
       return false; //prevent text selection
     })
@@ -35,6 +38,10 @@ $(function () {
       var show = document.getElementById("docmenu");
       show.style.visibility = "visible";
 
+      var all = $("highlighted").map(function() {
+            return this.innerHTML;
+        }).get();
+        console.log(all);
 
       isMouseDown = false;
     })
@@ -42,29 +49,31 @@ $(function () {
       if (isMouseDown) {
         $(this).toggleClass("highlighted");
       }
+
     });
+   
 });
 
 function left7() {
-  for(var b1 = 1; b1 <= 7; b1++) { //bugged
+  for(var b1 = 1; b1 <= 7; b1++) {
     var temp = parseInt(document.getElementById("a" + b1).innerHTML);
     document.getElementById("a" + b1).innerHTML = temp - 7;
   }
 }
 
   function right7() {
-    for(var b12 = 1; b12 <= 7; b12++) { //bugged
+    for(var b12 = 1; b12 <= 7; b12++) { 
       var temp1 = parseInt(document.getElementById("a" + b12).innerHTML);
       document.getElementById("a" + b12).innerHTML = temp1+ 7;
     }
   }
-function green(changeGreen){
+// function green(changeGreen){
 
-  // document.getElementById("ghl").bgcolor = "#7FFF00";
-  changeGreen.classList.toggle("green");
+//   // document.getElementById("ghl").bgcolor = "#7FFF00";
+//   changeGreen.classList.toggle("green");
 
-}
-function red(changeRed){
-  changeRed.classList.toggle("red");
+// }
+// function red(changeRed){
+//   changeRed.classList.toggle("red");
 
-}
+// }
