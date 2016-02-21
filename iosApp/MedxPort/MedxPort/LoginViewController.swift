08f2d512d632d9eca71fd4df47eef85c2ffe07a0
Malik <browne0@purdue.edu>
2016-02-20 22:25:07 -0500

    fixed dates and updated ios app

--- iosApp/MedxPort/MedxPort/LoginViewController.swift
+++ iosApp/MedxPort/MedxPort/LoginViewController.swift
@@ -16,14 +16,69 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var passwordLabel: JVFloatLabeledTextField!
     
+    @IBOutlet weak var errorLabel: UILabel!
+    
     let firebaseUrl = "https://medxport.firebaseio.com";
     
+    @IBOutlet weak var footerView: UIView!
+    
+    @IBOutlet weak var signInButton: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let emailImageView = UIImageView()
+        let emailImage = UIImage(named: "email")
         
+        emailImageView.frame = CGRect(x: 0, y: 0, width: (emailImage?.size.width)!+20.0, height: (emailImage?.size.height)!+10.0)
+        emailImageView.image = emailImage
+        emailImageView.contentMode = UIViewContentMode.Center
+        
+        usernameLabel.leftView = emailImageView
+        usernameLabel.leftViewMode = UITextFieldViewMode.Always
+        
+        let passwordImageView = UIImageView()
+        let passwordImage = UIImage(named: "password")
+        
+        passwordImageView.frame = CGRect(x: 0, y: 0, width: (passwordImage?.size.width)!+20.0, height: (passwordImage?.size.height)!+20.0)
+        passwordImageView.image = passwordImage
+        passwordImageView.contentMode = UIViewContentMode.Center
+        
+        passwordLabel.leftView = passwordImageView
+        passwordLabel.leftViewMode = UITextFieldViewMode.Always
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "medxport-bg")!)
+        
+        signInButton.layer.borderColor = UIColor.grayColor().CGColor
+        signInButton.layer.borderWidth = 0.0
+        signInButton.layer.cornerRadius = 5
+        signInButton.layer.masksToBounds = false
+        signInButton.layer.shadowRadius = 2.0
+        signInButton.layer.shadowColor = UIColor.blackColor().CGColor
+        signInButton.layer.shadowOffset = CGSizeMake(1.0, 1.0)
+        signInButton.layer.shadowOpacity = 1.0
+        signInButton.layer.shadowRadius = 1.0
+        
+        footerView.backgroundColor=UIColor.lightGrayColor().colorWithAlphaComponent(0.25)
+        
+        let border = CALayer()
+        let width = CGFloat(1.0)
+        border.borderColor = UIColor.lightGrayColor().CGColor
+        border.frame = CGRect(x: 0, y: usernameLabel.frame.size.height - width, width:  usernameLabel.frame.size.width, height: usernameLabel.frame.size.height)
+        
+        border.borderWidth = width
+        usernameLabel.layer.addSublayer(border)
+        usernameLabel.layer.masksToBounds = true
+        
+        let border2 = CALayer()
+        let width2 = CGFloat(1.0)
+        border2.borderColor = UIColor.lightGrayColor().CGColor
+        border2.frame = CGRect(x: 0, y: passwordLabel.frame.size.height - width2, width:  passwordLabel.frame.size.width, height: passwordLabel.frame.size.height)
+        
+        border2.borderWidth = width2
+        passwordLabel.layer.addSublayer(border2)
+        passwordLabel.layer.masksToBounds = true
     }
     
     @IBAction func onLogin(sender: AnyObject) {
@@ -35,15 +90,18 @@ class LoginViewController: UIViewController {
             withCompletionBlock: { error, authData in
                 if error != nil {
                     // There was an error logging in to this account
+                    UIView.animateWithDuration(0.3, animations: {
+                        self.errorLabel.alpha = 1.0
+                    })
                     print(error)
                 } else {
                     // We are now logged in
                     print("successful login")
+                    self.performSegueWithIdentifier("loginSegue", sender: sender)
                 }
         })
-        
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
